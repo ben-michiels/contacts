7210404c024cdc60088d571a541058fddf7a75fb
Ben Michiels <ben.michiels@gmail.com>
2021-08-21 14:24:54 +0100

    Pull and display users from API
    Generate the "My Contacts" page, with toolbar, non-functional search & sort features, and a list of users displayed from the data of the API.
    
    Approx. 7 hours into project.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,123 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="page-container"> 
+    
+    <md-app md-waterfall md-mode="fixed">
+      <md-app-toolbar md-theme="default-dark">
+        <div class="md-toolbar-row md-layout md-alignment-bottom-left">
+          <md-icon class="md-layout-item md-size-10">people</md-icon>
+          <span class="md-title md-layout-item">My Contacts</span>
+        </div>
+        
+        <div id="list-controls">
+          <div id="searching" class="md-toolbar-row md-layout md-alignment-center-center">
+            <p class="md-layout-item md-size-10 sorting-label">Search</p>
+            <md-field md-inline class="md-layout-item md-size-75 sorting-bar">
+              <md-input v-model="inline" md-layout="box" md-dense></md-input>
+            </md-field>
+          </div>
+          <div id="sorting" class="md-toolbar-row md-layout md-alignment-center-center">
+           <p class="md-layout-item md-size-10 sorting-label">Sort</p>
+            <md-field md-align-trigger class="md-layout-item md-size-75 sorting-bar">
+             <md-select v-model="sort" name="sort" id="sort">
+               <md-option value="first-name">First Name</md-option>
+               <md-option value="surname">Surname</md-option>
+               <md-option value="country">Country</md-option>
+             </md-select>
+            
+           </md-field>
+          </div>
+        </div>
+      </md-app-toolbar>
+
+        <md-app-content>
+        <div id="scrolling-list">
+          <md-list class="md-double-line">
+            <!-- input cards here -->
+            <div v-for="user of users" v-bind:key="user.login.uuid">
+              <contact-item v-bind:user="user"></contact-item>
+            </div>
+          </md-list>                       
+      </div>
+      </md-app-content>
+    </md-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import contactItem from './components/contact-item.vue'
+import axios from "axios";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    contactItem
+  },
+  data: () => ({
+    sort: 'first-name',
+    users: [],
+  }),
+  mounted() {
+    axios
+      .get("https://randomuser.me/api/?results=50&nat=gb")
+      .then((response) => (this.users = response.data.results));
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000000;
+  margin-top: 0px;
+}
+.md-app{
+  height:100vh
+}
+.md-app-content {
+  padding:0;
+}
+.md-icon {
+  margin: 0em;
+  margin-left: 1em;
+  color:white;
+}
+.md-title {
+  margin-top: 1.1em;
+  color:white;
+}
+.md-app-toolbar {
+  gap: 1em;
+  min-height: 5em;
+  background-color: #212121;
+  padding:0;
+}
+.md-toolbar-row {
+  align-content: flex-end;
+}
+#list-controls {
+  padding-top: 1em;
+  background-color: #f4f4f4;
+  min-width: 100%;
+}
+#searching,#sorting {
+  margin-left:0;
+  max-width: 100%;
+  gap: 10%;
+}
+.sorting-bar {
+  background-color: white;
+  padding:0.5em;
+}
+.sorting-label {
+  padding-bottom:1.2em;
+}
+#scrolling-list {
+  background-color:white;
+  min-height:100%;
+}
+.md-list {
+  padding:0;
 }
 </style>
---
+++ src/components/contact-item.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="contact-card">
+    <md-list-item>
+        <md-avatar>
+          <img v-bind:src="user.picture.thumbnail" alt="People">
+        </md-avatar>
+
+        <div class="md-list-item-text">
+          <span class="contact-name">{{`${user.name.first} ${user.name.last}`}}</span>
+          <span class="contact-details">{{`${user.location.state} / ${user.location.country}`}}</span>
+        </div>
+
+        <md-icon>chevron_right</md-icon>
+    </md-list-item>
+    <md-divider class="md-inset"></md-divider>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'contactItem',
+  props: 
+  ['user']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.contact-name {
+  color: #000000 !important;
+  font-size: 1em !important;
+}
+.contact-details {
+  color: #ff4081 !important;
+  font-size: 0.90em !important;
+}
+.contact-card {
+  background-color: white;
+
+}
+</style>
